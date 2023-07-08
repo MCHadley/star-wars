81e5ccfd8af69c995958617d08a24b5841d14c29
Michael <michael.c.hadley@gmail.com>
2023-07-07 20:03:35 -0400

    Add handling for searching and invalid response

--- api/handler.js
+++ api/handler.js
@@ -4,36 +4,44 @@
 const axios = require('axios');
 const {responseHandler, swapi} = require('../util/Utils');
 
-const getStarships = async () => {
+const getStarships = async (event, context) => {
   try {
     const starships = swapi('people');
-    const response = await axios.get(starships, {params: {search: 'Luke Skywalker'}});
-    const starshipsData = response.data.results[0].starships;
-    let starshipsList = [];
-    for (const [key, value] of Object.entries(starshipsData)) {
-      const res = await axios.get(value);
-      starshipsList.push(res.data);
+    const response = await axios.get(starships, {params: {search: event.queryStringParameters.search}});
+    if (response.data.count > 0) {
+      const starshipsData = response.data.results[0].starships;
+      let starshipsList = [];
+      for (const [key, value] of Object.entries(starshipsData)) {
+        const res = await axios.get(value);
+        starshipsList.push(res.data);
+      }
+      return responseHandler(200, starshipsList);
+    } else {
+      return responseHandler(400, 'There was an error with your request');
     }
-    return responseHandler(200, starshipsList);
   } catch (err) {
     console.error(`An error has occured: ${err}`);
     return responseHandler(400, `There was an error: ${err}`);
   }
 };
 
-const getSpecies = async () => {
+const getSpecies = async (event, context) => {
   try {
     const episodeOne = swapi('films');
-    const response = await axios.get(episodeOne, {params: {search: 'The Phantom Menace'}});
-    const species = response.data.results[0].species;
-    let speciesSet = new Set();
-    for (const [key, value] of Object.entries(species)) {
-      const res = await axios.get(value);
-      const classification = res.data.classification;
-      speciesSet.add(classification);
+    const response = await axios.get(episodeOne, {params: {search: event.queryStringParameters.search}});
+    if (response.data.count > 0) {
+      const species = response.data.results[0].species;
+      let speciesSet = new Set();
+      for (const [key, value] of Object.entries(species)) {
+        const res = await axios.get(value);
+        const classification = res.data.classification;
+        speciesSet.add(classification);
+      }
+      const speciesArray = Array.from(speciesSet);
+      return responseHandler(200, speciesArray);
+    } else {
+      return responseHandler(400, 'There was an error with your request');
     }
-    const speciesArray = Array.from(speciesSet);
-    return responseHandler(200, speciesArray);
   } catch (err) {
     console.error(`An error has occured: ${err}`);
     return responseHandler(400, `There was an error: ${err}`);
