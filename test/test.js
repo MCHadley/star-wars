a8b55d78db8fe3090ad12009b1a75900afab3e1c
Michael <michael.c.hadley@gmail.com>
2023-07-10 18:33:52 -0400

    Add tests

--- test/test.js
+++ test/test.js
@@ -4,33 +4,54 @@ const axios = require('axios');
 const chai = require('chai');
 const assert = chai.assert;
 
-describe('Test Starships endpoint', () =>{
-  it('should fetch Luke Skywalkers starships', async () => {
-    const res = await axios.get('http://localhost:3000/starships', {params: {search: 'Luke Skywalker'}});
+describe('Test Starships endpoint', () => {
+  const request = axios.get('http://localhost:3000/starships');
+  it('Returns 200', async () => {
+    const res = await request;
     assert.equal(res.status, 200);
+  });
+
+  it('returns X-wing and Imperial shuttle', async () => {
+    const res = await request;
     const starshipNames = res.data.message.map((starship) => starship.name);
-    const pilots = res.data.message.map((starship) => starship.pilots);
     assert.include(starshipNames, 'X-wing');
     assert.include(starshipNames, 'Imperial shuttle');
-    assert.include(pilots[0], 'https://swapi.dev/api/people/1/');
-    assert.include(pilots[1], 'https://swapi.dev/api/people/1/');
+  });
+
+  it('returns only 2 ships', async () => {
+    const res = await request;
+    assert.lengthOf(res.data.message, 2);
   });
 });
 
-describe('Test species endpoint', () =>{
-  it('Should return a list of 6 species', async () => {
-    const res = await axios.get('http://localhost:3000/species', {params: {search: 'The Phantom Menace'}});
+describe('Test species endpoint', () => {
+  const request = axios.get('http://localhost:3000/species');
+  it('Returns 200', async () => {
+    const res = await request;
     assert.equal(res.status, 200);
-    assert.lengthOf(res.data.message, 6);
+  }, 50000);
+
+  it('Returns classification includes', async () => {
+    const res = await request;
     assert.include(res.data.message, 'mammal', 'artificial', 'unknown', 'amphibian', 'mammals', 'reptile');
+  }, 50000);
+
+  it('Returns length is only 6', async () => {
+    const res = await request;
+    assert.lengthOf(res.data.message, 6);
   }, 50000);
 });
 
 describe('Test population endpoint', () =>{
-  it('Should a return a calcuation of all the species in the galaxy', async () => {
-    const res = await axios.get('http://localhost:3000/population');
-    assert.equal(res.status, 200);
-    assert.equal(res.data.message, 1711401432500);
-  }, 50000);
-});
+  const request = axios.get('http://localhost:3000/population');
 
+  it('Returns 200', async () => {
+    const res = await request;
+    assert.equal(res.status, 200);
+  }, 50000);
+
+  it('Returns number equals 1711401432500', async () => {
+    const res = await request;
+    assert.equal(res.data.message, 1711401432500);
+  });
+});
